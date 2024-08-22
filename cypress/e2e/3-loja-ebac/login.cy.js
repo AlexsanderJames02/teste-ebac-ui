6a47af14d45d928d1ffe17504d1c838f7b061967
Alexsander James <felicianoalex402@gmail.com>
2024-08-22 12:22:12 -0300

    Massa de dados e Fixture

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 ///<reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy. visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy. visit('minha-conta')
     })
 
     afterEach(() => {
@@ -14,7 +15,7 @@ describe('Funcionalidade: Login', () => {
         cy.get('#username').type ('alex.teste2204@teste.com.br')
         cy.get('#password').type ('teste@123')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, alex.teste2204 (não é alex.teste2204? Sair)')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá,')
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
@@ -31,4 +32,20 @@ describe('Funcionalidade: Login', () => {
         cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail alex.teste2204@teste.com.br está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error').should('exist')
     });
-})
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type (perfil.usuario)
+        cy.get('#password').type (perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá,')
+    });
+
+    it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+       cy.fixture('perfil').then( dados => {
+            cy.get('#username').type (dados.usuario)
+            cy.get('#password').type (dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá,')
+        });
+       })
+    });
